7d7402188f8dfed58e65af880030090fc19243a1
Dmitry <bens1iller@gmail.com>
2020-11-21 01:00:35 +0300

    fix problem with navigation presentation in page-header block

--- src/components/cakes-header/cakes-header.js
+++ src/components/cakes-header/cakes-header.js
@@ -1,19 +1,29 @@
 import React, {useState} from 'react';
 
 const Header = () => {
-    const [isShown, setIsShown] = useState(true);
+    const [isShown, setIsShown] = useState(false);
     const toggleFIeldset = () => setIsShown(!isShown);
 
     return (
         <header className="page-header container" id="header">
             <div className="page-header__title-burger">
                 <p className="page-header__title">Cakes</p>
-                <div className="main-nav__toggle" onClick={toggleFIeldset}>
+                <div className="main-nav__toggle" onClick={toggleFIeldset} >
                     <button className="main-nav__toggle-strike">Открыть меню</button>
                 </div>
             </div>
-            {isShown && 
             <div className="navigation">
+                <ul>
+                    <li><a href="#our-cakes">Наши торты</a></li>
+                    <li><a href="#individual-order">Индивидуальный заказ</a></li>
+                    <li><a href="#comments">Отзывы</a></li>
+                    <li><a href="#about-us">О нас</a></li>
+                    <li><a href="#how-order">Как заказать</a></li>
+                    <li><a href="#contacts">Контакты</a></li>
+                </ul>
+            </div>
+            {isShown && 
+            <div className="navigation navigation--mobile">
                 <ul>
                     <li><a href="#our-cakes">Наши торты</a></li>
                     <li><a href="#individual-order">Индивидуальный заказ</a></li>
